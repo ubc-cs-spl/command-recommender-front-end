0ba38b379c2bed24f08af20f83af4266e172f254
Ke Er Xiong <krx.sky@gmail.com>
2014-08-29 15:27:19 -0700

    ui improvement for reports
    Change-Id: I4c82e7c822aff907d18f874b0bd9629eb8470de6

--- app/scripts/controllers/report_controller.js
+++ app/scripts/controllers/report_controller.js
@@ -16,6 +16,8 @@ angular.module('frontEndApp')
                   NavigationService,
                   ReportService,
                   RecommendationService) {
+            
+            var ALGORITHM = 'MOST_WIDELY_USED'; //'mixed'  for supporting recommendations from different algorithms
 
             var modalOptions = {
                 templateUrl: 'views/modals/edit-command.html',
@@ -26,18 +28,27 @@ angular.module('frontEndApp')
             //TODO: refactor
             NavigationService.updateNavigation($routeParams.userId, 'report');
 
+            var getHslColor = function(h, s, l){
+                return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+            };
+
             $scope.validUser = {};
             $scope.period = 7;
 
             $scope.options = {
                 responsive : true,
                 dblclickAction : function(segment) {
-                    CommandService.retrieveCommand(segment.id).then(function() {
-                        modalInstance = $modal.open(modalOptions);
-                        modalInstance.result.then(function(commandUnderEdit){
+                    if (segment.id === 'other') {
+                        $scope.showOther = true;
+                        $scope.$apply();
+                    } else {
+                        CommandService.retrieveCommand(segment.id).then(function () {
+                            modalInstance = $modal.open(modalOptions);
+                            modalInstance.result.then(function (commandUnderEdit) {
 
-                        })
-                    });
+                            })
+                        });
+                    }
                 }
             };
 
@@ -45,19 +56,19 @@ angular.module('frontEndApp')
                 labels : [],
                 datasets : [
                     {
-                        label: "Command invocation not using shortcuts",
-                        fillColor: "rgba(220,220,220,0.5)",
-                        strokeColor: "rgba(220,220,220,0.8)",
-                        highlightFill: "rgba(220,220,220,0.75)",
-                        highlightStroke: "rgba(220,220,220,1)",
+                        label: 'Command invocation not using shortcuts',
+                        fillColor: 'rgba(220,220,220,0.5)',
+                        strokeColor: 'rgba(220,220,220,0.8)',
+                        highlightFill: 'rgba(220,220,220,0.75)',
+                        highlightStroke: 'rgba(220,220,220,1)',
                         data: []
                     },
                     {
-                        label: "Command invocations using shortcuts",
-                        fillColor: "rgba(151,187,205,0.5)",
-                        strokeColor: "rgba(151,187,205,0.8)",
-                        highlightFill: "rgba(151,187,205,0.75)",
-                        highlightStroke: "rgba(151,187,205,1)",
+                        label: 'Command invocations using shortcuts',
+                        fillColor: 'rgba(151,187,205,0.5)',
+                        strokeColor: 'rgba(151,187,205,0.8)',
+                        highlightFill: 'rgba(151,187,205,0.75)',
+                        highlightStroke: 'rgba(151,187,205,1)',
                         data: []
                     }
                 ]
@@ -65,9 +76,11 @@ angular.module('frontEndApp')
 
             var init = function() {
                 $scope.initialized = false;
+                $scope.showOther = false;
                 $scope.reports = [];
                 $scope.newlyLearnedCommands = [];
                 $scope.commandStatChartData = [];
+                $scope.otherCommandStatChartData = [];
                 $scope.colors = [];
                 $scope.report = [];
                 $scope.barchartData.labels =[];
@@ -87,56 +100,68 @@ angular.module('frontEndApp')
                 init();
                 ReportService.getReport($routeParams.userId, $scope.period).then(function(data) {
                     var stats = data.stats;
-                    if (stats.length > 0) {
-                        $scope.isStatsAvailable = true;
-                    } else {
-                        $scope.isStatsAvailable = false;
-                    }
+                    $scope.isStatsAvailable = stats.length > 0;
                     var totalCount = data.total_invocation;
+                    var otherCount = 0;
                     var hue = 0;
+                    var topCommandsCount = 0;
                     for(var i=0; i<stats.length; i += 1){
                         var cmd = stats[i];
                         var name = cmd.name || 'Missing';
                         var shortcut = cmd.shortcut;
                         var useCount = cmd.use_count;
                         var hotkeyCount = cmd.hotkey_count;
+                        var sectionData = {
+                            value: useCount,
+                            critical: hotkeyCount,
+                            color: getHslColor(hue, 90, 50),
+                            highlight: getHslColor(hue, 100, 65),
+                            label: name,
+                            id : cmd._id
+                        };
+                        if (useCount/totalCount < 0.01) {
+                            otherCount = totalCount - topCommandsCount;
+                            $scope.otherCommandStatChartData.push(sectionData);
+                        } else {
+                            topCommandsCount += useCount;
+                            $scope.commandStatChartData.push(sectionData);
+                        }
                         if (cmd.new) {
                             $scope.newlyLearnedCommands.push({
                                 name: name,
                                 shortcut: shortcut
                             })
                         }
-                        $scope.commandStatChartData.push({
-                            value: useCount,
-                            critical: hotkeyCount,
-                            color: $scope.getHslColor(hue, 90, 57),
-                            highlight: $scope.getHslColor(hue, 100, 50),
-                            label: name,
-                            id : cmd._id
-                        });
                         hue += 360 / totalCount * useCount;
-                        $scope.barchartData.labels.push(name + (shortcut ? " (" + shortcut + ")" : ""));
+                        $scope.barchartData.labels.push(name + (shortcut ? ' (' + shortcut + ')' : ''));
                         $scope.barchartData.datasets[0].data.push(useCount - hotkeyCount);
                         $scope.barchartData.datasets[1].data.push(hotkeyCount);
                     }
+                    $scope.commandStatChartData.push({
+                        value: otherCount,
+                        critical: otherCount,
+                        color: getHslColor(0, 0, 50),
+                        highlight: getHslColor(0, 0, 65),
+                        label: 'Other',
+                        id : 'other'
+                    })
                     $scope.initialized = true;
                 })
             };
             $scope.getReport();
 
-            $scope.getHslColor = function(h, s, l){
-                return "hsl(" + h + "," + s + "%," + l + "%)";
-            };
-
             $scope.getRecommendations = function() {
                 RecommendationService
-                    .getRecommendations($routeParams.userId, "current",
-                    "MOST_WIDELY_USED").then(function(data) {
+                    .getRecommendations($routeParams.userId, 'current',
+                    ALGORITHM).then(function(data) {
                         if (data != null) {
                             $scope.recommendations = data;
+                            $scope.rec = data[0];
                         }
                     });
             };
             $scope.getRecommendations();
-
+            $scope.setRec = function(recommendation) {
+                $scope.rec = recommendation;
+            };
         }]);
